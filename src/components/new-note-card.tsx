8c62ebf09514df0fda82d5bba36c66690a6d755e
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-04-01 02:01:43 -0300

    feat: :sparkles: show text area and add submit function to NewNote component buttons

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,10 +1,38 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { XIcon } from "lucide-react";
+import { useState } from "react";
 
 export function NewNoteCard() {
+  const [shouldShowOnBoarding, setShouldShowOnboarding] = useState(true);
+  const [content, setContent] = useState("")
+
+  function handleNewNote() {
+    setShouldShowOnboarding(true);
+  }
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false);
+  }
+
+  function handleContentChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setContent(event.target.value)
+
+    if (event.target.value == "") {
+      setShouldShowOnboarding(true);
+    }
+  }
+
+  function HandleSaveNote(event: React.FormEvent) {
+    event.preventDefault()
+    console.log(content)
+  }
+
   return (
     <Dialog.Root>
-      <Dialog.Trigger className="flex flex-col rounded-md bg-slate-700 text-left p-5 gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+      <Dialog.Trigger
+        onClick={handleNewNote}
+        className="flex flex-col rounded-md bg-slate-700 text-left p-5 gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400"
+      >
         <span className="text-sm font-medium text-slate-200">
           Adicionar nota
         </span>
@@ -20,29 +48,42 @@ export function NewNoteCard() {
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <XIcon className="size-5" />
           </Dialog.Close>
-          <div className="flex flex-1 flex-col gap-3 p-5">
-            <span className="text-sm font-medium text-slate-300">
-              Adicionar nota
-            </span>
+          <form onSubmit={HandleSaveNote} className="flex-1 flex flex-col">
+            <div className="flex flex-1 flex-col gap-3 p-5">
+              <span className="text-sm font-medium text-slate-300">
+                Adicionar nota
+              </span>
 
-            <p className="text-sm leading-6 text-slate-400">
-              Comece{" "}
-              <button className="font-medium text-lime-400 hover:underline ">
-                gravando uma nota
-              </button>{" "}
-              em áudio ou se preferir{" "}
-              <button className="font-medium text-lime-400 hover:underline">
-                utilize apenas texto.
-              </button>
-            </p>
-          </div>
+              {shouldShowOnBoarding == true ? (
+                <p className="text-sm leading-6 text-slate-400">
+                  Comece{" "}
+                  <button className="font-medium text-lime-400 hover:underline ">
+                    gravando uma nota
+                  </button>{" "}
+                  em áudio ou se preferir{" "}
+                  <button
+                    onClick={handleStartEditor}
+                    className="font-medium text-lime-400 hover:underline"
+                  >
+                    utilize apenas texto.
+                  </button>
+                </p>
+              ) : (
+                <textarea
+                  autoFocus
+                  className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                  onChange={handleContentChange}
+                />
+              )}
+            </div>
 
-          <button
-            type="button"
-            className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none hover:bg-lime-500"
-          >
-            Salvar nota
-          </button>
+            <button
+              type="submit"
+              className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none hover:bg-lime-500"
+            >
+              Salvar nota
+            </button>
+          </form>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
