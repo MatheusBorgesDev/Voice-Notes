69f696bae32df4e0f1b8f7af1168546812c5eecc
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-04-01 21:14:48 -0300

    feat: :sparkles: save notes on local storage and display all on main page

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,7 +3,11 @@ import { XIcon } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { toast } from "sonner";
 
-export function NewNoteCard() {
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void;
+}
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnBoarding, setShouldShowOnboarding] = useState(true);
   const [content, setContent] = useState("");
 
@@ -25,7 +29,11 @@ export function NewNoteCard() {
 
   function HandleSaveNote(event: FormEvent) {
     event.preventDefault();
-    console.log(content);
+
+    onNoteCreated(content);
+
+    setContent("");
+    setShouldShowOnboarding(true);
 
     toast.success("Nota criada com sucesso!");
   }
@@ -76,6 +84,7 @@ export function NewNoteCard() {
                   autoFocus
                   className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
                   onChange={handleContentChange}
+                  value={content}
                 />
               )}
             </div>
